7334edcce7c94ed9e62ba7ea0b2de4eb0875ede2
duc.nguyenminh <minhduc0400@gmail.com>
2022-05-18 00:21:31 +0700

    update api

--- index.js
+++ index.js
@@ -17,6 +17,35 @@ const {
 
 const app = express();
 
+app.get('/get-song/:songId', async (req, res, next) => {
+    const songId = req.params.songId;
+    const song = await getSong(songId);
+    return res.json(song);
+});
+
+app.get('/search/:key', async (req, res, next) => {
+    const key = req.params.key;
+    const result = await search(key);
+    return res.json(result);
+});
+
+app.get('/get-info-song/:songId', async (req, res, next) => {
+    const songId = req.params.songId;
+    const infoSong = await getInfoSong(songId);
+    return res.json(infoSong);
+});
+
+app.get('/get-lyric-song/:songId', async (req, res, next) => {
+    const songId = req.params.songId;
+    const lyricSong = await getLyric  (songId);
+    return res.json(lyricSong);
+});
+
+app.get('/get-chart', async (req, res, next) => {
+    const chart = await getChartHome();
+    return res.json(chart);
+});
+
 app.get('/get-top-100', async (req, res, next) => {
     const top100 = await getTop100();
     return res.json(top100);
@@ -30,7 +59,7 @@ app.get('/get-detail-playlist/:playlistId', async (req,res,next) =>{
 
 app.get('/', (req, res, next) => {
     return res.json({
-        name: "ehjasjdads"
+        name: "app"
     });
 });
 
