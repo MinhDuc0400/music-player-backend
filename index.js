e041a7016907509f53ab7884bff095f2db7352af
Đức Nguyễn <ducnguyen@ucs-MacBook-Pro.local>
2022-05-22 23:40:53 +0700

    update api

--- index.js
+++ index.js
@@ -20,13 +20,43 @@ const app = express();
 app.get('/get-song/:songId', async (req, res, next) => {
     const songId = req.params.songId;
     const song = await getSong(songId);
-    return res.json(song);
+    const songInfo = await getInfoSong(songId);
+    if (!song.err) {
+        let data = {
+            id: songInfo.data.encodeId,
+            title: songInfo.data.title,
+            thumbnail: songInfo.data.thumbnailM,
+            url: song.data['128'],
+            time: songInfo.data.duration,
+            artists: songInfo.data.artistsNames 
+        }
+        return res.json(data);
+    }
+});
+
+app.get('get-artist/:name', async(req, res, next) => {
+    const name = req.params.name;
+    const result = await getArtist(name);
+    return res.json(result);
 });
 
 app.get('/search/:key', async (req, res, next) => {
     const key = req.params.key;
     const result = await search(key);
-    return res.json(result);
+    const artistList = result.data.artists.map(el => ({
+        id: el.id,
+        name: el.name,
+        thumbnail: el.thumbnailM,
+        playlistId: el.playlistId,
+        totalFollow: el.totalFollow
+    }));
+    const songList = result.data.songs.map(el => ({
+        id: el.encodeId,
+        title: el.title,
+        artists: el.artistsNames,
+        thumbnail: el.thumbnailM
+    }));
+    return res.json({artistList, songList});
 });
 
 app.get('/get-info-song/:songId', async (req, res, next) => {
@@ -37,7 +67,7 @@ app.get('/get-info-song/:songId', async (req, res, next) => {
 
 app.get('/get-lyric-song/:songId', async (req, res, next) => {
     const songId = req.params.songId;
-    const lyricSong = await getLyric  (songId);
+    const lyricSong = await getLyric(songId);
     return res.json(lyricSong);
 });
 
@@ -48,13 +78,27 @@ app.get('/get-chart', async (req, res, next) => {
 
 app.get('/get-top-100', async (req, res, next) => {
     const top100 = await getTop100();
-    return res.json(top100);
+    let data = top100.data[0].items;
+    data = data.map(el => ({
+        title: el.title,
+        encodeId: el.encodeId,
+        thumbnail: el.thumbnail,
+        description: el.sortDescription
+    }));
+    return res.json(data);
 });
 
 app.get('/get-detail-playlist/:playlistId', async (req,res,next) =>{
     const id = req.params.playlistId;
     const playList = await getDetailPlaylist(id);
-    return res.json(playList);
+    // let data = playList.data;
+    let data = playList.data.song.items.map(el => ({
+        id: el.encodeId,
+        title: el.title,
+        artists: el.artistsNames,
+        thumbnail: el.thumbnailM
+    }));
+    return res.json(data);
 })
 
 app.get('/', (req, res, next) => {
